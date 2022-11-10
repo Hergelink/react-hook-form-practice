404051cde9f9973517b2a4f1d7094f6843b2a0a8
Hergelink <enver.hergelink@gmail.com>
2022-11-10 17:52:57 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
+// yup & react-hook tutorial
+// https://dev.to/franciscomendes10866/react-form-validation-with-react-hook-form-and-yup-4a98
+
 function App() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm({
+    name: '',
+    email: '',
+    userMessage: '',
+  });
+
+  console.log(errors);
+  // watch tracks only the given variable and does not cause a re-render! so it could be useful for displaying the entered value elsewhere!!!
+  // console.log(watch('name'))
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <div className='formContainer'>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log(data);
+          })}
         >
-          Learn React
-        </a>
-      </header>
+          <label htmlFor='name'>
+            Name
+            <input
+              type='text'
+              {...register('name', { required: 'This is required' })}
+              placeholder='Enter your name'
+            />
+          </label>
+          <p>{errors.name?.message}</p>
+          <label htmlFor=''>
+            Surname
+            <input
+              type='email'
+              {...register('email', { required: 'This is required' })}
+              placeholder='test@gmail.com'
+            />
+          </label>
+          <p>{errors.email?.message}</p>
+          <label htmlFor=''>
+            Message
+            <textarea
+              {...register('userMessage', {
+                required: 'please enter your message',
+                minLength: {
+                  value: 5,
+                  message: 'please enter more than 5 characters',
+                },
+              })}
+              cols='30'
+              rows='10'
+            ></textarea>
+          </label>
+          <p>{errors.userMessage?.message}</p>
+          <input type='submit' />
+          <button onClick={() => { reset()}}>reset</button>
+        </form>
+      </div>
     </div>
   );
 }
